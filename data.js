3e6a1c3791a5fa03210f89606b654e7f1e44a11d
harshpatel-27 <harshpatel2782008@gmail.com>
2025-02-24 20:50:04 +0530

    first

---
+++ data.js
@@ -0,0 +1,77 @@
+[
+    {
+        "question": "What does JDBC stand for?",
+        "opt1": "Java Database Communication",
+        "opt2": "Java Database Connectivity",
+        "opt3": "Java Data Control",
+        "opt4": "Java Dynamic Connection",
+        "ans": 2
+    },
+    {
+        "question": "Which of the following is not a JDBC component?",
+        "opt1": "DriverManager",
+        "opt2": "Statement",
+        "opt3": "ConnectionPool",
+        "opt4": "ResultSet",
+        "ans": 3
+    },
+    {
+        "question": "JDBC is used for:",
+        "opt1": "Connecting Java applications to a database",
+        "opt2": "Writing Java applets",
+        "opt3": "Compiling Java programs",
+        "opt4": "Managing server logs",
+        "ans": 1
+    },
+    {
+        "question": "Which package contains JDBC classes?",
+        "opt1": "java.jdbc",
+        "opt2": "javax.sql",
+        "opt3": "java.sql",
+        "opt4": "org.jdbc",
+        "ans": 3
+    },
+    {
+        "question": "What is the first step to establish a connection in JDBC?",
+        "opt1": "Load the driver",
+        "opt2": "Create a Statement",
+        "opt3": "Execute a query",
+        "opt4": "Close the connection",
+        "ans": 1
+    },
+    {
+        "question": "JDBC architecture consists of:",
+        "opt1": "JDBC API and JDBC Driver Manager",
+        "opt2": "JDBC API, Driver Manager, and Driver",
+        "opt3": "JDBC API, Driver, and Database",
+        "opt4": "JDBC API, Driver Manager, Driver, and Database",
+        "ans": 4
+    },
+    {
+        "question": "What is the purpose of DriverManager in JDBC?",
+        "opt1": "Manage database queries",
+        "opt2": "Establish connection with database driver",
+        "opt3": "Execute SQL statements",
+        "opt4": "Store query results",
+        "ans": 2
+    },
+    {
+        "question": "How many types of JDBC drivers are there?",
+        "opt1": "2",
+        "opt2": "3",
+        "opt3": "4",
+        "opt4": "5",
+        "ans": 3
+    },
+    {
+        "question": "Which of the following is not a type of JDBC driver?",
+        "opt1": "JDBC-ODBC Bridge Driver",
+        "opt2": "Thin Driver",
+        "opt3": "Native-API Driver",
+        "opt4": "JavaFX Driver",
+        "ans": 4
+    },
+  
+
+
+]
